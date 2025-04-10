10833f5b9c5d17dd969197250e09230ee1fb9529
Limon <victoriacuomo11@gmail.com>
2025-04-09 23:38:36 -0300

    feat: funciones basicas para funcionalidades del problema

---
+++ index.py
@@ -0,0 +1,126 @@
+
+
+
+
+
+#FUNCIONALIDAD 1: mostrar listado de artistas
+def mostrarListadoArtistas():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+def elegirBanda():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+# FUNCIONALIDAD 2 - elegir fecha
+def elegirFecha(artista):
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+# FUNCIONALIDAD 3 - elegir asientos
+
+def elegirUbicacion():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+def elegirAsientosEnUbicacion():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+# FUNCIONALIDAD 4 - ingresar datos
+
+def agregarDatosDeReserva():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+def buscarReserva():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+def elegirMedioDePago():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+# FUNCIONALIDAD 5 - 
+def pagarEntrada():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+def cargarEntradaDeCliente(cliente):
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+# FUNCIONALIDAD 6 - confirmar e imprimir boleta y entrada
+def imprimirBoleta():
+    '''
+    Que hace
+    numero de transaccion random
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+def imprimirEntrada():
+    '''
+    Que hace
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+
+# Programa principal
+
+def main():
+    '''
+    Que hace (menu de usuario)
+    Que parametros tiene
+    que retorna 
+    '''
+    pass 
+
+
+
+
+
+
