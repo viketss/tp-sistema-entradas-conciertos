b05ac1d2b094c49f59f2b9a28b642f6599b2303b
Tomas Flory <florytomas@protonmail.com>
2025-04-10 14:17:01 -0300

    seleccion_fecha

--- index.py
+++ index.py
@@ -35,11 +35,17 @@ def elegirFecha(artista):
     ]
     for i in range (len(artistas)):
         if artistas[i] == artista:
-            fechas_disponibles = fecha_por_artista
+            fechas_disponibles = fecha_por_artista[i]
             break
     
+    for i, fecha in enumerate(fechas_disponibles, start = 1):
+        print(f"{i}. {fecha}")
+    seleccionar_fecha = int(input("Por favor, ingrese el numero de fecha escogido"))
+    while seleccionar_fecha < 1 or seleccionar_fecha > len(fechas_disponibles):
+        print("Fecha invalida, intente nuevamente")
 
-
+    fecha_elegida = fechas_disponibles[seleccionar_fecha - 1]
+    return fecha_elegida
 # FUNCIONALIDAD 3 - elegir asientos
 
 def elegirUbicacion():
